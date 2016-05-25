edd6eced38a685df118460c48dab93c4c75bce02
Joopkins <jobndingdier@gmail.com>
2016-05-25 12:16:13 -0400

    feat: Add View for main list

---
+++ app/src/main/java/com/b3sk/rallygenius/View/SignListView.java
@@ -0,0 +1,10 @@
+package com.b3sk.rallygenius.View;
+
+/**
+ * Created by Joopkins on 5/25/16.
+ */
+public interface SignListView {
+    void ShowSigns(List<Sign> signs);
+    void ShowSignDetail(Sign sign);
+    void ShowSignSession();
+}
