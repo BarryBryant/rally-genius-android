e80543023dd72da64eee5f2c98301a8d519dc4c4
Joopkins <jobndingdier@gmail.com>
2016-05-29 23:58:04 -0400

    feat: Add info text for all signs

--- app/src/main/java/com/b3sk/rallygenius/Fragments/SignInfoActivityFragment.java
+++ app/src/main/java/com/b3sk/rallygenius/Fragments/SignInfoActivityFragment.java
@@ -7,6 +7,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.b3sk.rallygenius.R;
 
@@ -27,10 +28,16 @@ public class SignInfoActivityFragment extends Fragment {
         View rootView = inflater.inflate(R.layout.fragment_sign_info, container, false);
 
         ImageView imageView = (ImageView) rootView.findViewById(R.id.sign_image);
+        TextView textView = (TextView) rootView.findViewById(R.id.sign_info);
 
         TypedArray signImgs = this.getResources().obtainTypedArray(R.array.signs);
+        TypedArray signDesc = this.getResources().obtainTypedArray(R.array.sign_descriptions);
+
         imageView.setImageDrawable(signImgs.getDrawable(getArguments().getInt(SIGN_INDEX)));
+        textView.setText(signDesc.getText(getArguments().getInt(SIGN_INDEX)));
+
         signImgs.recycle();
+        signDesc.recycle();
 
         return rootView;
     }
